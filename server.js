1ec5a1a99b88f54c941ea5d63fcfa1192c1ce299
Cory Phillips <cory.phillips@sync1systems.com>
2019-05-13 01:08:08 -0500

    fixed a typo

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/angular-node-ngrx/index.html'));
 });
 
-app.get('/coupons', retrieveCoupon, function(req,res) {\
+app.get('/coupons', retrieveCoupon, function(req,res) {
 
 });
 
